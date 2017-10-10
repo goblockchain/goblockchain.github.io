a2eddb757e499fd282477e160c6034bcafdd4000
Henrique Goncalves Leite <henrique.leite@mercadolivre.com>
2017-10-10 20:26:43 -0300

    update site

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,16 @@ $(function() {
         $(".navbar-collapse").collapse('hide');
     });
 })
+
+function toggle_visibility(id) {
+    var thelist = document.getElementsByClassName("alist");
+    for (var i = 0; i < thelist.length; i++) {
+        thelist[i].style.display = 'none';
+    }
+    var e = document.getElementById(id);
+    if(e.style.display == 'block') {
+        e.style.display = 'none';
+    } else {
+        e.style.display = 'block';
+    }
+}
