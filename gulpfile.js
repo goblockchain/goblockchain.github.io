a1ceda86a20738b0e0893c275a0cc0c5b1fe3f86
Victor Alves <victor.alves@mprj.mp.br>
2018-10-03 11:24:28 -0300

    separating directory from the application of others

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
+    gulp.watch("./app/*.html").on("change", reload);
+    gulp.watch("app/*.js").on("change", reload);
+    gulp.watch("app/css/*.css").on("change", reload);
+    gulp.watch("app/scss/*.css").on("change", reload);
+});
